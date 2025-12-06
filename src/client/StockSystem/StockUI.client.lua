c119b4f3e27e31530f1af2717127084b18cc58b1
dev1 <haimenguyen@gmail.com>
2025-12-05 20:11:46 -0800

    :sparkles: added ui feature

--- src/client/StockSystem/StockUI.client.lua
+++ src/client/StockSystem/StockUI.client.lua
@@ -5,6 +5,26 @@ local player = Players.LocalPlayer
 local playerGui = player:WaitForChild("PlayerGui")
 local stockUI = playerGui:WaitForChild("StockUI")
 
+local closeButton = stockUI:FindFirstChild("CloseButton", true)
+
+if not closeButton then
+	local timeout = 10
+	local elapsed = 0
+	while not closeButton and elapsed < timeout do
+		task.wait(0.1)
+		closeButton = stockUI:FindFirstChild("CloseButton", true)
+		elapsed += 0.1
+	end
+end
+
+if closeButton then
+	closeButton.MouseButton1Click:Connect(function()
+		stockUI.Enabled = false
+	end)
+else
+	warn("CloseButton not found in StockUI")
+end
+
 local openEvent = ReplicatedStorage:WaitForChild("OpenStockUI")
 openEvent.OnClientEvent:Connect(function()
 	stockUI.Enabled = true
